cc218b87c47daa8e5c9439ea0c793a2dbcf82491
imxnotxbatman-cloud <imxnotxbatman@gmail.com>
2026-05-07 00:00:58 -0600

    project start

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myrigidbody;
+    public float flapStrength;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myrigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
+
+    }
+}
+
+
